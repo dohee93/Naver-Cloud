cd34b76ff571377cf30df34d7944cf84220e1842
dohee93 <rlaehgml03@gmail.com>
2021-03-19 13:18:30 +0900

    footer.js 추가하여 js 하나로 통일

--- cloud/js/cloud.js
+++ cloud/js/cloud.js
@@ -237,43 +237,6 @@ function scrollToTop() {
 
 
 
-
-
-
-// footer Button JS
-
-var footerRight = document.querySelectorAll('.family-site');
-var familyButton = document.querySelectorAll('.family-btn');
-
-var familyOpen = false;
-
-function familyClick(idx) {
-   familyButton[idx].addEventListener('click',function(){
-      // alert(idx);
-      if(familyOpen){
-         footerRight[idx].classList.remove('on');
-         familyOpen = false;
-      }
-      else{
-         footerRight[idx].classList.add('on');
-         familyOpen = true;
-      }
-   });
-}
-
-for(var i = 0; i < familyButton.length; i++) {
-   familyClick(i);
-}
-
-// ------ footer Button JS
-
-
-
-
-
-
-
-
 // Small Navigation JS
 // Header & Section Menu Fixed JS
 var content02Top = document.getElementById('content02').offsetTop;
