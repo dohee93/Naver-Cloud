2f970d218885836f67f692162462494ec918c6a1
dohee93 <rlaehgml03@gmail.com>
2021-03-22 18:20:43 +0900

    index.js

--- js/index.js
+++ js/index.js
@@ -91,21 +91,21 @@ var btt = document.getElementById('top-btn'),
 
    
 
-   var slideIndex = 1;
-showSlides(slideIndex);
+//    var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.querySelector(".mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
+// function showSlides(n) {
+//   var i;
+//   var slides = document.querySelector(".mySlides");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   slides[slideIndex-1].style.display = "block";  
 
-} 
+// } 
